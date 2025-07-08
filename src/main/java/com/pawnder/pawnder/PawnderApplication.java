a5b556da89e4664fcd13a01760261a562bc6f79b
thisisubin <bm1418109@gmail.com>
2025-07-08 16:20:31 +0900

    초기 세팅

---
+++ src/main/java/com/pawnder/pawnder/PawnderApplication.java
@@ -0,0 +1,13 @@
+package com.pawnder.pawnder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawnderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawnderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawnder/pawnder/PawnderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawnder.pawnder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
